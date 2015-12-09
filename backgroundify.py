e59e8e4979e89d296673347171bf2c8334d05344
David Gowers <finticemo@gmail.com>
2015-12-10 08:00:25 +1030

    New Layer (quick) command added to backgroundify.py.
    This command creates an image-sized layer with the specified name, mode, and opacity.
    For example, when inking a bunch of drawings, I just fill in name=Ink and mode=Multiply,
    then I can just hit Enter when it comes up again.
    
    It can be found at 'Layer->Stack->New Layer (quick)' in the menus.
    
    Created to fix three main deficiencies of the New Layer dialog:
      * I don't care about the dimensions. Let them be image sized, I'll crop the layer if memory demands require it.
      * Doesn't allow you to set the mode in the same operation
      * Not non-interactively repeatable (via F2)
    
    In the future, should also fix the following deficiency of the New Layer Dialog
      * Initial fill value is often strongly implied by mode
        (eg. Multiply or Darken Only: you probably want white.
        Add, Sub, Lighten Only or Screen, you probably want black.
        Grain merge/extract, soft or hard light or overlay,
        you probably want (128,128,128) grey.)
    
    This should be compatible with both 2.8 and 2.9. However, it hasn't been tested with 2.9 yet.

--- backgroundify.py
+++ backgroundify.py
@@ -5,6 +5,31 @@ from gimpfu import *
 gettext.install("gimp20-python", gimp.locale_directory, unicode=True)
 
 BGCOLOR, PATTERN = 0, 1
+MODES = (
+ (_('Normal'), NORMAL_MODE),
+ (_('Dissolve'), DISSOLVE_MODE),
+ (_('Lighten only'), LIGHTEN_ONLY_MODE),
+ (_('Screen'), SCREEN_MODE),
+ (_('Dodge'), DODGE_MODE),
+ (_('Addition'), ADDITION_MODE),
+ (_('Darken Only'), DARKEN_ONLY_MODE),
+ (_('Multiply'), MULTIPLY_MODE),
+ (_('Burn'), BURN_MODE),
+ (_('Overlay'), OVERLAY_MODE),
+ (_('Soft Light'), SOFTLIGHT_MODE),
+ (_('Hard Light'), HARDLIGHT_MODE),
+ (_('Difference'), DIFFERENCE_MODE),
+ (_('Subtract'), SUBTRACT_MODE),
+ (_('Grain Extract'), GRAIN_EXTRACT_MODE),
+ (_('Grain Merge'), GRAIN_MERGE_MODE),
+ (_('Divide'), DIVIDE_MODE),
+ (_('Hue'), HUE_MODE),
+ (_('Saturation'), SATURATION_MODE),
+ (_('Color'), COLOR_MODE),
+ (_('Value'), VALUE_MODE),
+ (_('Behind(?)'), BEHIND_MODE),
+)
+MODELIST = tuple(v[0] for v in MODES)
 
 def without_group_layers(layerlist):
     """Returns a recursively flattened list of layers.
@@ -16,6 +41,19 @@ def without_group_layers(layerlist):
         else:
             layers.append(l)
     return layers
+
+def new_layer(image, drawable, name, mode, opacity, alpha):
+    pdb.gimp_image_undo_group_start(image)
+    layertype = image.layers[0].type
+    mode = MODES[mode][-1]
+    layer = pdb.gimp_layer_new(image, image.width, image.height, layertype, name, opacity, mode)
+    pdb.gimp_drawable_fill(layer, WHITE_FILL)
+    pdb.gimp_image_insert_layer(image, layer, None, -1)
+    if alpha:
+        pdb.gimp_layer_add_alpha(layer)
+#    else:
+#        pdb.gimp_layer_remove_alpha(layer)
+    pdb.gimp_image_undo_group_end(image)
 
 def backgroundify(image, drawable, fillmode, all_layers):
     all_layers = (all_layers == 1)
@@ -33,6 +71,7 @@ def backgroundify(image, drawable, fillmode, all_layers):
         pdb.gimp_edit_bucket_fill_full(layer, bfill_mode, BEHIND_MODE, 100.0, 255.0, 0, 1, 0, 0, 0)
 
     pdb.gimp_image_undo_group_end(image)
+
 
 register(
     proc_name="python-fu-backgroundify",
@@ -57,6 +96,31 @@ register(
     menu=("<Image>/Layer/Transparency"), 
     domain=("gimp20-python", gimp.locale_directory)
     )
+
+register(
+    proc_name="python-fu-new-layer",
+    blurb="Quickly create a new image-sized layer with specified mode and opacity",
+    help=("..."),
+    author="David Gowers",
+    copyright="David Gowers",
+    date=("2015"),
+    label=("_New Layer(quick)..."),
+    imagetypes=("*"),
+    params=[
+            (PF_IMAGE, "image", "_Image", None),
+            (PF_LAYER, "drawable", "_Drawable", None),
+            (PF_STRING, "name", "_Name", "Layer"),
+            (PF_OPTION, "mode", "_Blending mode", 0,
+                        MODELIST),
+            (PF_SLIDER, "opacity", "_Opacity", 100, (0, 100, 1)),
+            (PF_BOOL, "alpha", "Alpha channel", 0),
+            ],
+    results=[],
+    function=new_layer,
+    menu=("<Image>/Layer/Stack"),
+    domain=("gimp20-python", gimp.locale_directory)
+    )
+
 
 
 main()
